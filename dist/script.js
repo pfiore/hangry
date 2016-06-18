e93ceb85179c532440b418f04659e46869f03f52
xkfngs <christopherstokes@gmail.com>
2016-06-17 19:48:34 -0700

    update server add routes

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,17 @@
-function getMyLocation() {
+function getLocation () {
     navigator.geolocation.getCurrentPosition(function(position) {
         console.log(position.coords.latitude, ' ', position.coords.longitude);
 
     });
 }
 
+function selectRandomPhrase (phraseArr) {
+    return phraseArr[Math.floor(Math.random() * phraseArr.length)];
+}
 
 var map;
 
-function initCenter(lat, lng) {
+function initCenter (lat, lng) {
     var mapCenter = {};
     mapCenter.lat = lat;
     mapCenter.lng = lng;
--- server.js
+++ server.js
@@ -7,15 +7,27 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
+
+// ROUTES
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.json({ message: 'yo'})
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    //do logging
+    console.log('Something is happening.');
+    next();
 });
+
+router.get('/', function(req, res) {
+    res.json({ message: 'yo'});
+});
+
+router.route('/loc')
+    .post(function(req, res) {
+    });
 
 app.use('/api', router);
 app.use(express.static('dist'));
 
 app.listen(port);
 console.log('listening on port ' + port);
-
