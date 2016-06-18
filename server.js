604cb65207ec5b35e9be5513c28dc14632a15955
xkfngs <christopherstokes@gmail.com>
2016-06-17 19:16:10 -0700

    server update

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
-
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'yo'})
+});
+
+app.use('/api', router);
 app.use(express.static('dist'));
 
-app.listen(3000, function() {
-    console.log('listening on port 3000');
-});
+app.listen(port);
+console.log('listening on port ' + port);
+
